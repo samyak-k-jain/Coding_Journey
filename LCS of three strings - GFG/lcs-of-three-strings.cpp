7928d296a9185725c43a8ea7194abe96754dbc87
samyak-k-jain <84249110+samyak-k-jain@users.noreply.github.com>
2022-07-14 11:36:04 +0530

    Added solution - LeetHub

---
+++ LCS of three strings - GFG/lcs-of-three-strings.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int LCSof3 (string A, string B, string C, int n1, int n2, int n3);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; cin >> n1 >> n2 >> n3;
+        string A, B, C; cin >> A >> B >> C;
+        cout << LCSof3 (A, B, C, n1, n2, n3) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
+
+
+int LCSof3 (string A, string B, string C, int n, int m, int l)
+{
+    int dp[n+1][m+1][l+1];
+    for(int i = 0; i < n+1; i++)
+    {
+        for(int j = 0; j < m+1; j++)
+        {
+            for(int k = 0; k < l+1; k++)
+            {
+                if(i==0||j==0||k==0)
+                {
+                    dp[i][j][k] = 0;
+                }
+            }
+        }
+    }
+    
+    for(int i = 1; i < n+1; i++)
+    {
+        for(int j = 1; j < m+1; j++)
+        {
+            for(int k = 1; k < l+1; k++)
+            {
+                if(A[i-1] == B[j-1] && B[j-1] == C[k-1])
+                    dp[i][j][k] = 1+dp[i-1][j-1][k-1];
+                else
+                {
+                    dp[i][j][k] = max(dp[i-1][j][k],max(dp[i][j-1][k],dp[i][j][k-1]));
+                }
+            }
+        }
+    }
+    return dp[n][m][l];
+}
