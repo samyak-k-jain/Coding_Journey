46cb363a76e09680025b4fa6b5f53febf36ace23
samyak-k-jain <84249110+samyak-k-jain@users.noreply.github.com>
2022-07-22 12:07:34 +0530

    Time: 41 ms (6.80%), Space: 21 MB (54.12%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -0,0 +1,26 @@
+
+class Solution
+{
+public:
+    bool isBalanced(TreeNode* root) 
+    {
+        return check(root)!=-1;
+    }
+    int check(TreeNode *root)
+    {
+        if(root == NULL)
+            return 0;
+        int lh = check(root->left);
+        if(lh == -1)
+            return -1;
+        
+        int rh = check(root->right);
+        if(rh == -1)
+            return -1;
+        
+        if(abs(lh-rh)>1) return -1;
+        
+        return 1 + max(lh,rh);
+        
+    }
+};
